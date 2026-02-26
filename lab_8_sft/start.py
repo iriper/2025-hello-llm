29b2c1f0269fbd1a6c04260c71278126d41f744b
Irina Pereyaslavtseva <irinapereyas@gmail.com>
2026-02-26 13:40:15 +0300

    added imports

--- lab_8_sft/start.py
+++ lab_8_sft/start.py
@@ -5,7 +5,10 @@ Fine-tuning starter.
 # pylint: disable=too-many-locals, undefined-variable, unused-import, too-many-branches, too-many-statements
 
 
+from pathlib import Path
 from core_utils.llm.time_decorator import report_time
+from core_utils.project.lab_settings import LabSettings
+from lab_8_sft.main import RawDataImporter, RawDataPreprocessor
 
 
 @report_time
